c94e7d2674ffa91e9315df2d6e183c68792f08dc
vvronc <ronovictor40@gmail.com>
2023-02-21 13:07:38 +0300

    created login page html&css

---
+++ signUp.js
@@ -0,0 +1,11 @@
+//login fileds
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const submitButton = document.getElementById("email");
+
+//create Account
+const signUpEmailIn = document.getElementById("email-signup");
+const confirmSignupEmailInput = document.getElementById("confirm-email-signup");
+const signUpPasswordIn = document.getElementById("password-signup")
+const confirmsignupPasswordIn = document.getElementById("confirm-password-signup")
+const createacctbtn = document.getElementById("create-acct-btn")
